759e11d465354e4d57310ed63760e8903cb06093
sehyun lee <mh080411@kyonggi.ac.kr>
2025-09-09 09:47:07 +0900

    프로젝트 생성
    React + Vite + tailwindcss 기반의 프로젝트 생성

---
+++ helios-admin/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="align-center flex justify-center m-10">Hello World!</div>
+    </>
+  );
+}
+
+export default App;
